56c3410dc1a056cdc5b1e6f37ae4f7195ab56e8c
Sarah Nur Azizah <Sarah Nur Azizah@zizah-eyd>
2023-08-28 13:35:53 +0700

    Sampling Form need improve

---
+++ Object Repository/Coba dulu/nutrisi.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>nutrisi</name>
+   <tag></tag>
+   <elementGuidId>41e4082a-6b9d-4f3c-a7af-8221944f7e29</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(//h5[.=&quot;Nutrisi&quot;]) [3]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
---
+++ Object Repository/Coba dulu/radio_belum hamil.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>radio_belum hamil</name>
+   <tag></tag>
+   <elementGuidId>cba5927e-2913-4dbc-a2af-8d4aad8c2769</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//p[.=&quot;Belum Hamil&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
