d9aa220c5c36066b43403c6b55c1e23deef4f21c
Shandika <Shandika@DESKTOP-NNQII6O>
2023-12-11 11:19:24 +0700

    commit open produk page

---
+++ Object Repository/Homepage/button_Saya Setuju.rs
@@ -0,0 +1,113 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>button_Saya Setuju</name>
+   <tag></tag>
+   <elementGuidId>88f14931-1e9d-404f-be5f-855ea22176cf</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>button.footer__notfication-btn.btn.btn-primary.p-2</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>(.//*[normalize-space(text()) and normalize-space(.)='Pelajari lebih lanjut'])[1]/following::button[1]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>button</value>
+      <webElementGuid>6a832361-f9a1-46ba-a029-5be6bc5ecdc3</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>footer__notfication-btn btn btn-primary p-2</value>
+      <webElementGuid>677ef264-506b-40ac-a856-c23d08b93984</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+                    Saya Setuju
+                </value>
+      <webElementGuid>c7a43bb2-6638-4728-b591-8117a0d9e792</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>/html[1]/body[1]/div[@class=&quot;footer__notification active&quot;]/div[@class=&quot;container&quot;]/div[@class=&quot;row&quot;]/div[@class=&quot;col-md-4 text-center align-self-center&quot;]/button[@class=&quot;footer__notfication-btn btn btn-primary p-2&quot;]</value>
+      <webElementGuid>9aa34dd9-b062-4e3f-9aca-7fac7d6e8c6c</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Pelajari lebih lanjut'])[1]/following::button[1]</value>
+      <webElementGuid>bc9d15cd-d0a9-4b04-8c3f-2dd266dc86de</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='cookies'])[1]/following::button[1]</value>
+      <webElementGuid>71a5d5ff-24f9-40ee-87c2-dbc0efec6eeb</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Asi terbaik bagi bayi'])[1]/preceding::button[1]</value>
+      <webElementGuid>2569cd3e-3258-4fea-a609-306fbf0833e2</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Syarat &amp; Ketentuan'])[1]/preceding::button[2]</value>
+      <webElementGuid>51aa365e-9bdc-4247-9fa5-92a740e877b0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Saya Setuju']/parent::*</value>
+      <webElementGuid>4dcb5293-2fb0-4e4f-bae2-6d39f7c64921</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//body/div/div/div/div[2]/button</value>
+      <webElementGuid>48281c34-67e4-47cd-8e6a-a9d07bdb4a64</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//button[(text() = '
+                    Saya Setuju
+                ' or . = '
+                    Saya Setuju
+                ')]</value>
+      <webElementGuid>1531b2e4-123f-4a42-93c4-61f5ac4c92a6</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
